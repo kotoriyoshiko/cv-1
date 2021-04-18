75ad5d7205f97de7f29d7cc893ee80cc6c039dcd
kotoriyoshiko <kotoriyoshiko@outlook.com>
2021-04-18 21:33:11 +0800

    233

---
+++ src/main.js
@@ -0,0 +1,81 @@
+let html = document.querySelector("#demo"); //通过CSS选择器找到#demo
+let style = document.querySelector("#style");
+let string = `
+/*你好，我是一名前端新人
+*接下来我要加内容了
+*首先准备一个div
+*/
+#div1{
+    width:400px;
+    height:400px;
+    position:fixed;
+    right:0;
+    top:0;
+    transform:translate(-100%)
+}
+/*接下来把div变成一个八卦
+*先把div变成一个圆
+*/
+#div1{
+    border-radius:50%;
+    box-shadow:0 0 3px rgba(0,0,0,0.5);
+    border:none; 
+}
+/*八卦是阴阳形成
+*/
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%);
+}
+/*加两个球
+*/
+#div1::before{
+    content:'';
+    display:black;
+    // border:1px solid red;
+    position:absolute;
+    width:200px;
+    height:200px;
+    left:25%;
+    top:0;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%);
+
+    
+}
+#div1::after{
+    content:'';
+    display:black;
+    // border:1px solid red;
+    position:absolute;
+    width:200px;
+    height:200px;
+    left:25%;
+    bottom:0;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 24%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
+}
+`;
+let string2 = "";
+let n = 0;
+
+let step = () => {
+  setTimeout(() => {
+    if (string[n] === "\n") {
+      string2 = string2 + "<br>";
+    } else if (string[n] === " ") {
+      string2 = string2 + "&nbsp";
+    } else {
+      string2 = string2 + string[n];
+    }
+    html.innerHTML = string2;
+    style.innerHTML = string.substring(0, n);
+    window.scroll(0, 9999);
+    html.scroll(0, 9999);
+    if (n < string.length - 1) {
+      n = n + 1;
+      step();
+    }
+  }, 10);
+};
+
+step();
